af23746b5da71b3c6f03342964deede5257ab9ca
Raymond <wrg7yu@gmail.com>
2020-09-20 22:28:10 -0400

    Add functionality to take photo, store it, and display it on a result view

--- app/src/main/java/com/example/teacheats/ResultFragment.kt
+++ app/src/main/java/com/example/teacheats/ResultFragment.kt
@@ -1,23 +1,45 @@
 package com.example.teacheats
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.findNavController
 import com.example.teacheats.databinding.FragmentResultBinding
-
+import java.io.File
+var result : String = ""
 class ResultFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        // Use the Kotlin extension in the fragment-ktx artifact
+        setFragmentResultListener("requestKey") { key, bundle ->
+            // We use a String here, but any type that can be put in a Bundle is supported
+            result = bundle.getString("bundleKey").toString()
+        }
         val binding = DataBindingUtil.inflate<FragmentResultBinding>(inflater, R.layout.fragment_result, container, false)
+        getImage(binding)
         binding.resultReturnButton.setOnClickListener{view: View ->
             view.findNavController().navigate(R.id.action_resultFragment_to_titleFragment)
         }
         return binding.root
+    }
+    //Finds image
+    private fun getImage(binding : FragmentResultBinding) {
+        if (result == "") {
+            return
+        }
+        val imgFile =  File(result)
+        if(imgFile.exists()){
+            val myImage : ImageView = binding.foodPicture;
+            myImage.setImageURI(Uri.fromFile(imgFile))
+        }
     }
 }
--- app/src/main/java/com/example/teacheats/TitleFragment.kt
+++ app/src/main/java/com/example/teacheats/TitleFragment.kt
@@ -1,45 +1,92 @@
 package com.example.teacheats
 
 import android.content.Intent
-import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.FileProvider
+import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.setFragmentResult
 import androidx.navigation.findNavController
-import com.example.teacheats.databinding.ActivityMainBinding
 import com.example.teacheats.databinding.FragmentTitleBinding
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TitleFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TitleFragment : Fragment() {
     private val REQUEST_IMAGE_CAPTURE = 1
+    private val REQUEST_TAKE_PHOTO = 1
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         //Inflates and binds layout to fragment
-        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
+            inflater,
+            R.layout.fragment_title,
+            container,
+            false
+        )
         //Binds listeners to buttons
-        binding.helpButton.setOnClickListener{view: View ->
+        binding.helpButton.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_titleFragment_to_helpFragment)
         }
-        binding.startButton.setOnClickListener{view: View ->
+        binding.startButton.setOnClickListener { view: View ->
+            dispatchTakePictureIntent()
+            setFragmentResult("requestKey", bundleOf("bundleKey" to currentPhotoPath))
             view.findNavController().navigate(R.id.action_titleFragment_to_resultFragment)
         }
         return binding.root
     }
+
+    lateinit var currentPhotoPath: String
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Creates image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = activity?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a filepath for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
     private fun dispatchTakePictureIntent() {
-        PackageManager packageManager = getActivity().getPackageManager()
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            takePictureIntent.resolveActivity(packageManager)?.also {
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            //ensure that there's a camera activity to handle the intent
+            activity?.packageManager?.let {
+                takePictureIntent.resolveActivity(it)?.also {
+                    // Create the File where the photo should go
+                    val photoFile: File? = try {
+                        createImageFile()
+                    } catch (ex: IOException) {
+                        // Error occurred while creating the File
+                        Log.d("Error", "Error")
+                        null
+                    }
+                    // Continue only if the File was successfully created
+                    photoFile?.also {
+                        val photoURI: Uri = FileProvider.getUriForFile(requireActivity(), "com.example.android.fileprovider", it)
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+//                        REQUEST_TAKE_PHOTO
+                        Log.d("Picture", "is working")
+                        startActivity(takePictureIntent)
+                    }
+                }
             }
         }
     }
